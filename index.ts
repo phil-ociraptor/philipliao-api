3ef9f8b308b34eb31dc4d901dc02f5543f13a804
Philip Liao <philipliao@gmail.com>
2016-02-22 22:12:02 -0800

    get typescript typings up

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 /// <reference path="typings/tsd.d.ts" />
-var express = require('express');
+
+import express = require('express');
 var app = express();
+
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+  res.send('Hello World!');
 });
+
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3000!');
 });
